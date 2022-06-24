2a3db1a43055b332bd796bc70c66084bab733121
Elozino A. Ovedhe <ovedhee@gmail.com>
2022-06-24 15:02:25 +0100

    dashboard-ui

---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashboardLayout from '../../components/DashBoard/DashboardLayout/DashboardLayout'
+
+const Profile = () => {
+  return (
+    <DashboardLayout>
+      <div>Profile</div>
+    </DashboardLayout>
+  )
+}
+
+export default Profile
