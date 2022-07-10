85da90f25dab8fb780aa34c94878c51f448031af
Elozino A. Ovedhe <ovedhee@gmail.com>
2022-07-10 18:43:16 +0100

    home nav responsiveness

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Button from '../Button/Button'
 import "./NavBar.css"
+import { TiThMenu } from "react-icons/ti"
 
 const NavBar = () => {
   let navigate = useNavigate();
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+  const [menu, setMenu] = useState(true)
+  function getWindowSize() {
+    const { innerWidth, innerHeight } = window;
+    return { innerWidth, innerHeight };
+  }
 
   const handleAuth = (e) => {
     e.preventDefault()
     navigate("/signin")
   }
 
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
   return (
     <nav className='navBar'>
       <div className='navBar__logo'>BANIWAZ</div>
-      <div className='navBar__navLink'>
+      <div className='navBar__navLink'
+        style={{ display: menu && "none" }}
+      >
         <ul>
           <li>
             <Link to='/'>Home</Link>
@@ -31,11 +52,26 @@ const NavBar = () => {
           <li>
             <Link to="/terms">Terms</Link>
           </li>
+          {windowSize.innerWidth <= "720" &&
+            <li>
+              <div onClick={handleAuth}>
+                <Button title="SIGN IN" color="white" />
+              </div>
+            </li>
+          }
         </ul>
       </div>
-      <div onClick={handleAuth}>
-        <Button title="SIGN IN" color="white" />
-      </div>
+
+      {windowSize.innerWidth <= "720" &&
+        <div onClick={() => setMenu(!menu)}>
+          <TiThMenu size="20" />
+        </div>
+      }
+      {windowSize.innerWidth > "720" &&
+        <div onClick={handleAuth}>
+          <Button title="SIGN IN" color="white" />
+        </div>
+      }
     </nav>
   )
 }
